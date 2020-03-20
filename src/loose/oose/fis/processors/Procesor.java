f0159b5bc1fd5ed2510bd25edb7f0b46d90a36d2
Stefanescu Adrian <stefanescuadrian04@gmail.com>
2020-03-20 15:29:30 +0200

    Updated NewBranch

--- src/loose/oose/fis/processors/Procesor.java
+++ src/loose/oose/fis/processors/Procesor.java
@@ -5,5 +5,5 @@ import loose.oose.fis.documents.Document;
 import java.util.ArrayList;
 
 public interface Procesor {
-    int proceseaza(ArrayList<Document> documente);
+    int proceseaza(ArrayList<Document> documente1);
 }
